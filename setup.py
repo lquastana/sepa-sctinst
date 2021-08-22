5e29d8e11906a6de83b64e7f90d944a14b899555
Laurent Quastana <laurent.quastana@gmail.com>
2021-08-22 04:35:53 +0200

    Improve setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name="sepa-sctinst",
       version="1.0a1",
-      description="Tools for SCTInst payment scheme",
+      description="SEPA SCTInst is a python package that provides some features to manage SCTInst payments",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/lquastana/sepa-sctinst",
       author="lquastana",
       packages=["sepa_sctinst"],
       install_requires=["lxml","Faker"],
       extras_require={
             "dev": ["requests-mock"]
       },
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+      keywords='sepa sctinst',
+      platforms='any',
       license="Apache 2.0")
