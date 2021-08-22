abbcdcd362724965fb005b236b1252953dc344d7
Laurent Quastana <laurent.quastana@gmail.com>
2021-08-22 14:46:05 +0200

    Autodetect message type

--- sepa_sctinst/__init__.py
+++ sepa_sctinst/__init__.py
@@ -1,36 +1,92 @@
 from sepa_sctinst.sct_inst import SCTInst
-
+from lxml import etree as ET
+import re
 
 class MessageConfiguration:
-   def  __init__(self,code:str,xsd_filepath:str):
+    def  __init__(self,code:str,xsd_filepath:str,xmlns:str):
        """MessageConfiguration constructor
 
        Args:
            code (str): Payment code
            xsd_filepath (str): XSD file path
+           xmlns (str): XML namespace
        """
        self.code = code
        self.xsd_filepath = xsd_filepath
+       self.xmlns = xmlns
+
+    @staticmethod
+    def autodetect(data):
+        """Autodetect type of message
+
+        Args:
+            data (str): XML Data
+
+        Returns:
+            MessageConfiguration: MessageConfiguration which match with the xmlns attribute
+        """
+        xmlns = None
+        xmlsn_regex = re.findall('xmlns=(".*?")',data)
+        
+        print("RESULT:"+str(len(xmlsn_regex)))
+       
+        if len(xmlsn_regex) > 0:
+            xmlns = xmlsn_regex[0].strip('"')
+            print(xmlsn_regex)
+        for attr in dir(Message):
+            if type(getattr(Message, attr)) == MessageConfiguration and getattr(Message, attr).xmlns == xmlns:
+                return getattr(Message, attr)
+                
         
 
 class Message:
     """ All messages in the SCTInst scheme
     """
-    SCTINST = MessageConfiguration(SCTInst,'sepa_sctinst/xsd/pacs.008.001.02.xsd')
-    POSITIVE_STATUS = MessageConfiguration('PositiveStatus','sepa_sctinst/xsd/pacs.002.001.03_Positive_Update.xsd')
-    NEGATIVE_STATUS = MessageConfiguration('NegativeStatus','sepa_sctinst/xsd/pacs.002.001.03_Negative.xsd')
-    SCTINST_STATUS_INV = MessageConfiguration('SctinstStatusInv','sepa_sctinst/xsd/pacs.028.001.01_Status.xsd')
+    SCTINST = MessageConfiguration(SCTInst,
+                                   'sepa_sctinst/xsd/pacs.008.001.02.xsd',
+                                   'urn:iso:std:iso:20022:tech:xsd:pacs.008.001.02')
+    POSITIVE_STATUS = MessageConfiguration('PositiveStatus',
+                                           'sepa_sctinst/xsd/pacs.002.001.03_Positive_Update.xsd',
+                                           'urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03')
+    NEGATIVE_STATUS = MessageConfiguration('NegativeStatus',
+                                           'sepa_sctinst/xsd/pacs.002.001.03_Negative.xsd',
+                                           'urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03')
+    SCTINST_STATUS_INV = MessageConfiguration('SctinstStatusInv',
+                                              'sepa_sctinst/xsd/pacs.028.001.01_Status.xsd',
+                                              'urn:iso:std:iso:20022:tech:xsd:pacs.028.001.01')
     
-    RECALL = MessageConfiguration('Recall','sepa_sctinst/xsd/camt.056.001.01_Recall.xsd')
-    RECALL_POS=MessageConfiguration('RecallPos','sepa_sctinst/xsd/pacs.004.001.02_PosAnRecall.xsd')
-    RECALL_NEG=MessageConfiguration('RecallNeg','sepa_sctinst/xsd/camt.029.001.03_NegAnRecall.xsd')
-    RECALL_STATUS_INV=MessageConfiguration('RecallStatusInv','sepa_sctinst/xsd/pacs.028.001.01_Recall_Update.xsd')
+    RECALL = MessageConfiguration('Recall',
+                                  'sepa_sctinst/xsd/camt.056.001.01_Recall.xsd',
+                                  'urn:iso:std:iso:20022:tech:xsd:camt.056.001.01')
+    RECALL_POS=MessageConfiguration('RecallPos',
+                                    'sepa_sctinst/xsd/pacs.004.001.02_PosAnRecall.xsd',
+                                    'urn:iso:std:iso:20022:tech:xsd:pacs.004.001.02')
+    RECALL_NEG=MessageConfiguration('RecallNeg',
+                                    'sepa_sctinst/xsd/camt.029.001.03_NegAnRecall.xsd',
+                                    'urn:iso:std:iso:20022:tech:xsd:camt.029.001.03')
+    RECALL_STATUS_INV=MessageConfiguration('RecallStatusInv',
+                                           'sepa_sctinst/xsd/pacs.028.001.01_Recall_Update.xsd',
+                                           'urn:iso:std:iso:20022:tech:xsd:028.001.01')
     
-    RFRO = MessageConfiguration('RFRO','sepa_sctinst/xsd/pacs.008.001.02.xsd')
-    RFRO_POS=MessageConfiguration('RfroPos','sepa_sctinst/xsd/pacs.004.001.02_PosReRFRO.xsd')
-    RFRO_NEG=MessageConfiguration('RfroNeg','sepa_sctinst/xsd/camt.029.001.03_NegReRFRO.xsd')
-    RFRO_STATUS_INV=MessageConfiguration('RfroStatusInv' ,'sepa_sctinst/xsd/pacs.028.001.01_RFRO_Update.xsd')
+    RFRO = MessageConfiguration('RFRO',
+                                'sepa_sctinst/xsd/camt.056.001.01_RFRO.xsd',
+                                'urn:iso:std:iso:20022:tech:xsd:camt.056.001.01')
+    RFRO_POS=MessageConfiguration('RfroPos',
+                                  'sepa_sctinst/xsd/pacs.004.001.02_PosReRFRO.xsd',
+                                  'urn:iso:std:iso:20022:tech:xsd:pacs.004.001.02')
+    RFRO_NEG=MessageConfiguration('RfroNeg',
+                                  'sepa_sctinst/xsd/camt.029.001.03_NegReRFRO.xsd',
+                                  'urn:iso:std:iso:20022:tech:xsd:camt.029.001.03')
+    RFRO_STATUS_INV=MessageConfiguration('RfroStatusInv' ,
+                                         'sepa_sctinst/xsd/pacs.028.001.01_RFRO_Update.xsd',
+                                         'urn:iso:std:iso:20022:tech:xsd:pacs.028.001.01')
     
-    SCTINST_C2B = MessageConfiguration('SCTInstC2B','sepa_sctinst/xsd/pain.001.001.03.xsd')
-    SCTINST_C2B_BACK = MessageConfiguration('SCTInstBackC2B','sepa_sctinst/xsd/pain.001.001.03_TransferBack.xsd')
-    SCTINST_C2B_STATUS = MessageConfiguration('SCTInstStatusC2B','sepa_sctinst/xsd/pain.002.001.03.xsd')
+    SCTINST_C2B = MessageConfiguration('SCTInstC2B',
+                                       'sepa_sctinst/xsd/pain.001.001.03.xsd',
+                                       'urn:iso:std:iso:20022:tech:xsd:pain.001.001.03')
+    SCTINST_C2B_BACK = MessageConfiguration('SCTInstBackC2B',
+                                            'sepa_sctinst/xsd/pain.001.001.03_TransferBack.xsd',
+                                            'urn:iso:std:iso:20022:tech:xsd:pain.001.001.03')
+    SCTINST_C2B_STATUS = MessageConfiguration('SCTInstStatusC2B',
+                                              'sepa_sctinst/xsd/pain.002.001.03.xsd',
+                                              'urn:iso:std:iso:20022:tech:xsd:pain.002.001.03')
--- tests/test_schema_validation.py
+++ tests/test_schema_validation.py
@@ -1,8 +1,9 @@
 from sepa_sctinst.schema_validation import SchemaValidation
-from sepa_sctinst import Message
+from sepa_sctinst import Message, MessageConfiguration
 from faker import Faker
 
 import pytest
+from lxml import etree
 
 fake = Faker()
 
@@ -17,17 +18,35 @@ message_empty = 'Document is empty'
         ('tests/sct_inst_files/pain001_valid.xml',Message.SCTINST_C2B, True, False,None)
     ]
 )
-def test_pacs008_xsd(filename,message_type,is_valid,error_msg_present,expected_message):
+def test_schema_validation(filename,message_type,is_valid,error_msg_present,expected_message):
 
     with open(filename, 'r') as input:
         data = input.read()
     response = schema_validation.validate(data,message_type)
 
     assert response['isValid'] == is_valid
-    
     assert ('error_messages' in response) == error_msg_present
 
     if expected_message is not None:
         messages_matched = filter(lambda x: expected_message in x, response['error_messages'])
         assert any(messages_matched)
+        
+        
+@pytest.mark.parametrize(
+    "filename,expected_message" ,[
+        ('tests/sct_inst_files/pacs008_valid.xml',Message.SCTINST),
+        ('tests/sct_inst_files/pacs008_invalid.xml',Message.SCTINST), 
+        ('tests/sct_inst_files/pain001_valid.xml',Message.SCTINST_C2B),
+        ('tests/sct_inst_files/pain001_invalid_xmlns.xml',None)
+    ]
+)
+def test_autodetect_xsd(filename,expected_message):
+
+    with open(filename, 'r') as input:
+        data = input.read()
+    message_type = None
+    message_type = MessageConfiguration.autodetect(data)
+        
+    assert message_type == expected_message
+        
 
