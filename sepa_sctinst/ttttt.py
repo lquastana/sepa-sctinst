dd698fed753754fce2ba8b28c1eeed0e35d7ccd0
Laurent Quastana <laurent.quastana@gmail.com>
2021-09-01 23:28:21 +0200

    delete unwanted file

--- sepa_sctinst/ttttt.py
+++
@@ -1,5 +0,0 @@
-from sepa_sctinst.sct_inst_interbank import SCTInst
-from sepa_sctinst.sct_inst_c2b import SCTInstC2B
-
-c2b_message = SCTInstC2B.random(nb_txs=4)
-interbank_message = SCTInst.random()
